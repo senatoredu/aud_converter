35a9e7d549d07a1df8a3c2525328e31f3afb3586
edu <e.chiedu@yahoo.com>
2020-05-09 19:56:35 +0200

    commit to local repo

---
+++ lambda_function.py
@@ -0,0 +1,22 @@
+import boto3
+import urllib3
+import certifi
+import re
+
+def lambda_handler(event, context):
+    sns = boto3.client('sns')
+    url = 'https://zar.currencyrate.today/aud/1000'
+    http = urllib3.PoolManager(ca_certs=certifi.where())
+    r =  http.request('GET', url)
+    r = r.data.decode('utf-8')
+    aud = re.findall(r'\$[0-9,.]+', r)
+    aud = ''.join(aud)
+    message = 'Hello Edu, Right now 1000 Rands is', aud
+
+    topicArn = 'arn:aws:sns:eu-west-1:123456789:EXCHANGE'
+
+    sns.publish(
+        TopicArn = topicArn,
+        Message = message
+   )
+
